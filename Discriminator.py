fbed9a1568bfe17b5898a71599096b0ea01082bf
unknown <rajatkulkarni48@gmail.com>
2020-11-08 16:44:13 +0530

    First Commit

--- Discriminator.py
+++ Discriminator.py
@@ -3,25 +3,29 @@ import torch
 import torch.nn as nn
 class Discriminator(nn.Module):
     
-    def __init__(self, input_size, hidden_size,output_size):
+    def __init__(self, input_size, hidden_size,output_size,n_layers):
         super(Discriminator, self).__init__()
         self.input_size=input_size
         self.hidden_size=hidden_size
         self.output_size=output_size
+        self.n_layers=n_layers
         self.embed=nn.Embedding(CONFIG.vocab_size,self.input_size)
-        self.lstm=nn.LSTM(self.input_size,self.hidden_size,batch_first=True)
+        self.lstm=nn.LSTM(self.input_size,self.hidden_size,self.n_layers,batch_first=True)
+        self.dropout = nn.Dropout(p=0.5)
         self.l1=nn.Linear(self.hidden_size,self.output_size)
         self.prob=nn.Sigmoid()
 
     def init_hidden(self, batch_size):
-      return(torch.autograd.Variable(torch.randn(1, batch_size, self.hidden_size)),torch.autograd.Variable(torch.randn(1, batch_size, self.hidden_size)))   
+      weight = next(self.parameters()).data
+      hidden = (weight.new(self.n_layers, batch_size, self.hidden_size).zero_(),
+                      weight.new(self.n_layers, batch_size, self.hidden_size).zero_())  
+      return hidden  
         
-    def forward(self, x):
-      self.hidden = self.init_hidden(x.size(0))
+    def forward(self, x,hidden):
       embed_x=self.embed(x)
-      embed_x=embed_x.reshape(x.size(0),self.input_size,-1)
-      out,(ht,ct)=self.lstm(embed_x,self.hidden)
-      ht=torch.squeeze(ht)
-      output1=self.l1(ht)
-      prob=self.prob(output1)
-      return prob
+      lstm_out,(ht,ct)=self.lstm(embed_x,hidden)
+      lstm_out=lstm_out[:,-1,:]
+      out = self.dropout(lstm_out)
+      out_l1 = self.l1(out)
+      sig_out = self.prob(out_l1)
+      return sig_out,hidden
