84d75a972b169204f2d309038dcba14939727958
unknown <rajatkulkarni48@gmail.com>
2020-11-08 16:43:49 +0530

    First commit

--- CONFIG.py
+++ CONFIG.py
@@ -4,9 +4,10 @@ note_to_int = pickle.load(f)
 val_list = list(note_to_int.values())
 
 epochs=20
-batch_size=3
+batch_size=16
 vocab_size=len(val_list)
-input_size=10
+input_size=20
 hidden_size=50
-gen_output_size=10
+gen_output_size=20
 dis_output_size=1
+n_layers=1
